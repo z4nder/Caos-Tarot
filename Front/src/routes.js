f8615a3943b70ba1677bdb17b9f39417a80a899e
gustavo.silva <gustavo.silva@codificar.com.br>
2020-03-22 00:40:21 -0300

    Add icons to menu

--- Front/src/routes.js
+++ Front/src/routes.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React, {Component} from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 import OneCard from './pages/Readings/OneCard';
 const Tab = createBottomTabNavigator()
@@ -9,7 +10,23 @@ const Tab = createBottomTabNavigator()
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;        
+                        if (route.name === 'Uma Carta') {
+                        iconName = focused
+                            ? 'ios-eye'
+                            : 'ios-eye-outline';
+                        }         
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'grey',
+                    inactiveTintColor: 'gray',
+                }}
+            >
                 <Tab.Screen name="Uma Carta" component={OneCard} />
             </Tab.Navigator>
         </NavigationContainer>
