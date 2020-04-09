9bd7135ae016247f4bf55c8e77ea0e8c74bd3c7c
gustavo.silva <gustavo.silva@codificar.com.br>
2020-04-08 21:46:15 -0300

    Fix reading  and use FlatList

--- Front/src/pages/Readings/DrawnCards/index.js
+++ Front/src/pages/Readings/DrawnCards/index.js
@@ -1,80 +1,113 @@
 import React, { Component } from 'react';
 import Card from '../../../components/Card'
-import {View, TouchableOpacity, ScrollView, Text, TextInput} from 'react-native';
+import { View, TouchableOpacity, ScrollView, SafeAreaView, FlatList } from 'react-native';
 import { Bars } from 'react-native-loader';
 import FullButton from '../../../components/FullButton'
 
 // Styles
 import styles from "./DrawnCards.style";
 class DrawnCards extends Component {
-    constructor(props) { 
-        super(props);         
+    constructor(props) {
+        super(props);
         this.state = {
-            isLoading: false,
-            cardSelected: [],
+            isLoading: true,
+            cardsData: [],           
             cardQuantity: 0,
-            showCard: []
+            items: []
         };
-    }   
-
-    async componentDidMount(){
-        this.setState({
-           cardQuantity: this.props.route.params.cardQuantity
-        })     
     }
 
-    renderCards() { 
-        let items = []        
-        for (let index = 0; index < this.state.cardQuantity; index++) {
-            let randomNumber = this.getRandomArbitrary(21)
-            items.push(
-                <TouchableOpacity 
-                    style={styles.clickContent}
-                    onPress={() => this.drawnCard(index)}
-                >
-                    <Card 
-                        number={randomNumber}
-                        showCard={this.state.showCard[index]}
-                    />
-                </TouchableOpacity>  
-            )          
-        }      
-        
-		return items;	
-	}	    
+    async componentDidMount() {
 
-    drawnCard(index) {        
-        let show = this.state.showCard
-        show[index] = true
+    }
+
+    async componentWillMount() {
+        this.renderCards()
+    }
+
+    renderCards() {
+        let items =  []        
+        for (let index = 0; index < this.props.route.params.cardQuantity; index++) {
+            let content =  {}
+            content.id = index
+            content.cardPosition = this.getRandomArbitrary(21) 
+            content.showCard = false    
+
+            items.push(content)
+        }
+        console.log("items", items);
         this.setState({
-            showCard: show
-        })
+            isLoading: false,
+            cardsData: items,
+        })   
+    }
+   
+    drawnCard(index) {
+        let content = this.state.cardsData
+        console.log("this.state.cardsData", this.state.cardsData);        
+        content[index].cardFliped = true
+        this.setState({
+            cardsData: content,
+        })       
     }
 
     getRandomArbitrary(max) {
         return Math.floor(Math.random() * max);
     }
 
-    render (){
-        return(
-            <View  style={styles.container}>
-                
+    // haveEqualsValue(newNumber) {
+    //     for (let index = 0; index < this.state.cardQuantity.length; index++) {
+    //         if(this.state.cardQuantity[index] == newNumber) return true         
+    //     }
+    //     return false
+    //  }    
+
+    //  getRandomArbitrary(max) {        
+    //      let equals = false
+    //      let number = null
+    //      do {
+    //          number = Math.floor(Math.random() * max)
+    //          equals = this.haveEqualsValue(number)
+    //      } while (!equals);
+
+    //      return number;
+    //  }
+
+    render() {
+        return (
+            <View style={styles.container}>
+
                 <View style={styles.content}>
-                {this.state.isLoading
-                ?
-                    <Bars size={10} color="#111110"/>
-                    
-                :   
-                    <ScrollView horizontal={true}>          
-                        {this.renderCards()}
-                    </ScrollView>
-                }
+                    {this.state.isLoading
+                        ?
+                        <Bars size={10} color="#111110" />
+
+                        :
+                        <ScrollView horizontal={true}>                            
+                                <FlatList
+                                    horizontal={true}
+                                    data={this.state.cardsData}
+                                    renderItem={({ item, index }) => (
+                                        <TouchableOpacity
+                                            style={styles.clickContent}
+                                            onPress={() => this.drawnCard(index)}
+                                        >
+                                            <Card
+                                                number={item.cardPosition}
+                                                showCard={item.cardFliped}
+                                            />
+                                        </TouchableOpacity>
+                                    )}
+                                    keyExtractor={item => item.id}
+                                />
+                        </ScrollView>
+                    }
                 </View>
-            </View> 
-            
-           
-        )      
-   }
+            </View>
+
+
+        )
+    }
 
 }
 
